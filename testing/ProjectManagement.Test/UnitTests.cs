e3085fac735560cf8777654737a4cdf53f61260d
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-15 00:01:34 -0700

    Sample unit test case is added

---
+++ testing/ProjectManagement.Test/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestM()
+        {
+            int x = 10;
+            int y = 20;
+            Assert.AreEqual(30, x + y);
+        }
+    }
+}
