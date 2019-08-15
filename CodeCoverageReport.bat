0c5f56ebfd41ab8ac26e4b37b02a33b69547b28e
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-14 23:55:31 -0700

    Batch file is added

---
+++ CodeCoverageReport.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+
+rem CodeCoverageReport using ReportGenerator
+
+"%~dp0packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe" -reports:TestResult.xml -targetdir:"CodeCoverage-OpenCover"
+
+exit
---
+++ PerformanceTest.bat
@@ -0,0 +1,7 @@
+﻿@echo off
+
+rem Performance Test
+
+"%~dp0packages\NBench.Runner.0.2.1\lib\net45\NBench.Runner.exe" testing\ProjectManagement.PerformanceTest\bin\Release\ProjectManagement.PerformanceTest.dll
+
+exit
