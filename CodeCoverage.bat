05cb0692102f7c7e03b8c893afd0a45245488b7a
Selvam Subramani <muthuselvam1987@gmail.com>
2019-08-15 11:41:58 -0700

    exclude attribute is added

--- CodeCoverage.bat
+++ CodeCoverage.bat
@@ -2,6 +2,6 @@
 
 rem CodeCoverage using Open Cover
 
-"%~dp0packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" -targetargs:"/noisolation /testcontainer:testing\ProjectManagement.Test\bin\Release\ProjectManagement.Test.dll" -register:user -filter:"+[ProjectManagement.Service]* -[ProjectManagement.BusinessLayer]*" -output:TestResult.xml -excludebyattribute:"*ExcludeFromCodeCoverage" -mergebyhash -skipautoprops
+"%~dp0packages\OpenCover.4.6.166\tools\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" -targetargs:"/noisolation /testcontainer:testing\ProjectManagement.Test\bin\Release\ProjectManagement.Test.dll" -register:user -filter:"+[ProjectManagement.Service]* -[ProjectManagement.BusinessLayer]*" -output:TestResult.xml -excludeByAttribute:"System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage" -mergebyhash -skipautoprops
 
 exit
